13b443e662b2a86f29fe1f23c320e24f326d292f
Mohammad Rafe <mrrafi5869@gmail.com>
2022-11-24 09:15:02 +0600

    project setup

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBs9TQvouLgFRvgI7-mKs-lJO4CmENYLXk",
+  authDomain: "used-products-resale-fa9a4.firebaseapp.com",
+  projectId: "used-products-resale-fa9a4",
+  storageBucket: "used-products-resale-fa9a4.appspot.com",
+  messagingSenderId: "440390545050",
+  appId: "1:440390545050:web:456afe096403f052d37923"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
