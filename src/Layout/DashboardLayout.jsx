e17ec9f12605edbb6f557afd288b03f1683590f0
Mohammad Rafe <mrrafi5869@gmail.com>
2022-11-27 09:59:55 +0600

    seller

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -1,41 +1,57 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 import Footer from "../components/Shared/Footer/Footer";
 import SharedNavbar from "../components/Shared/SharedNavbar";
+import { AuthContext } from "../contexts/AuthProvider";
+import useAdmin from "../hooks/useAdmin";
 
 const DashboardLayout = () => {
+  const { user } = useContext(AuthContext);
+  const [isAdmin] = useAdmin(user?.email);
+
   return (
     <>
-        <SharedNavbar></SharedNavbar>
-    <div className="w-9/12 mx-auto">
+      <SharedNavbar></SharedNavbar>
+      <div className="w-9/12 mx-auto">
         <div className="drawer drawer-mobile">
-      <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        <Outlet></Outlet>
+          <input
+            id="dashboard-drawer"
+            type="checkbox"
+            className="drawer-toggle"
+          />
+          <div className="drawer-content">
+            <Outlet></Outlet>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="dashboard-drawer"
+              className="drawer-overlay"
+            ></label>
+            <ul className="menu p-4 w-80 text-base-content">
+              <li>
+                <Link to="/dashboard">Add a Product</Link>
+              </li>
+              <li>
+                <Link to="/dashboard/myOrders">My Orders</Link>
+              </li>
+              {isAdmin && (
+                <>
+                  <li>
+                    <Link to="/dashboard/allSellers">All Sellers</Link>
+                  </li>
+                  <li>
+                    <Link to="/dashboard/allBuyers">All Buyers</Link>
+                  </li>
+                  <li>
+                    <Link to="/dashboard/allUsers">All Users</Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="drawer-side">
-        <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-80 text-base-content">
-          <li>
-            <Link to='/dashboard'>Add a Product</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/myOrders">My Orders</Link>
-          </li>
-          <li>
-            <Link to='/dashboard/allUsers'>All Users</Link>
-          </li>
-          <li>
-            <Link to="/dashboard/allSellers">All Sellers</Link>
-          </li>
-          <li>
-            <Link to='/dashboard/allBuyers'>All Buyers</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-    </div>
-    <Footer></Footer>
+      <Footer></Footer>
     </>
   );
 };
